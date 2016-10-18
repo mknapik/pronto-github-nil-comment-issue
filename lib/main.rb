fc7947bebdbd3b70b4c3bdf8c46dc2bc7fcf6750
Michał Knapik <michal.knapik@u2i.com>
2016-10-18 17:15:14 +0200

    Violations in one line

--- lib/main.rb
+++ lib/main.rb
@@ -1,13 +1,9 @@
 # frozen_string_literal: true
 # Docs
 class Fibonacci
-
-
   def self.call(n)
-
+    key     = nil
     Hash.new { |hash, key| hash[key] = hash[key - 2] + hash[key - 1] }
         .tap { |hash| hash[0] = hash[1] = 1 }[n]
   end
-
-
 end
